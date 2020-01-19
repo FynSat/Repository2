8b1f42d9545bda012c8718bdd01c4d708053fc6e
fynsatdlf@gmail.com <fynsatdlf@gmail.com>
2020-01-20 00:11:58 +0530

    Commit2

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -6,6 +6,7 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine( "Hi");
             Console.WriteLine("Hello World!");
         }
     }
